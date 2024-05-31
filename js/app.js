9fdcda1d2d14daca1ea38f16a09df4935f796a8a
Charlie Jay <charliejbjay@gmail.com>
2024-05-31 16:24:53 +1000

    added a new gamePlay function

--- js/app.js
+++ js/app.js
@@ -1,17 +1,23 @@
-console.log('LessssGooooo!');
+// I want to create a function that iterates through the cats array at random
+// I want to have a play button
+// The play button will call the function that iterates over the cats array
+// I want a function that 
 
 /*-------------------------------- Constants --------------------------------*/
-const mainElement = document.querySelector('.main');
-const resetButtonElement = document.querySelector('#reset');
+// const gamePlayOptions = 
 
 /*---------------------------- Variables (state) ----------------------------*/
-
+let main;
+let gameOver;
 /*------------------------ Cached Element References ------------------------*/
-
+const lilCatsElement = document.querySelectorAll('.cats');
+const resultDisplayElement = document.querySelector('#message');
+const mainElement = document.querySelector('.main');
+const resetButtonElement = document.querySelector('#reset');
 /*-------------------------------- Functions --------------------------------*/
 
 function init() {
-
+    // gamePlay();
 }
 
 function render() {
@@ -19,13 +25,25 @@ function render() {
 }
 
 function handleClick(event) {
-    console.log('clicked!');
+    // console.log('clicked!');
+}
+
+function gamePlay() {
+    lilCatsElement.forEach(cat => {
+        console.log(cat.id);
+    });
 }
 
 function reset() {
-    console.log('clicked!');
+    // console.log('clicked!');
     init();
 }
+
+// function updateMessage() {
+//     if (winner === false) {
+//         resultDisplayElement.textContent = 'Play?';
+//     }
+// }
 /*----------------------------- Event Listeners -----------------------------*/
 
 document.addEventListener('DOMContentLoaded', init);
