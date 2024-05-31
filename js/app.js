0de30929410beb556aef4e8ad1e22b0214d16645
Charlie Jay <charliejbjay@gmail.com>
2024-05-31 14:13:22 +1000

    Added some basic JS

--- js/app.js
+++ js/app.js
@@ -1,1 +1,35 @@
 console.log('LessssGooooo!');
+
+/*-------------------------------- Constants --------------------------------*/
+const mainElement = document.querySelector('.main');
+const resetButtonElement = document.querySelector('#reset');
+
+/*---------------------------- Variables (state) ----------------------------*/
+
+/*------------------------ Cached Element References ------------------------*/
+
+/*-------------------------------- Functions --------------------------------*/
+
+function init() {
+
+}
+
+function render() {
+    
+}
+
+function handleClick(event) {
+    console.log('clicked!');
+}
+
+function reset() {
+    console.log('clicked!');
+    init();
+}
+/*----------------------------- Event Listeners -----------------------------*/
+
+document.addEventListener('DOMContentLoaded', init);
+
+mainElement.addEventListener('click', handleClick);
+
+resetButtonElement.addEventListener('click', reset);
